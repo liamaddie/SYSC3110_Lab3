2d2f245d76014a845dd7b6d5b402d14d04cc6def
liamaddie <liamaddie@cmail.carleton.ca>
2025-09-19 14:14:40 -0400

    I created a buddy and added them to an addressBook then removed them. I also renamed the class field addressBook to myBuddies.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,21 +3,24 @@ import java.util.List;
 
 public class AddressBook
 {
-    private List<BuddyInfo> addressBook;
+    private List<BuddyInfo> myBuddies;
 
     public AddressBook() {
-        addressBook = new ArrayList<>();
+        myBuddies = new ArrayList<>();
     }
 
     public boolean addBuddy(BuddyInfo aBuddy) {
-        return addressBook.add(aBuddy);
+        return myBuddies.add(aBuddy);
     }
 
     public boolean removeBuddy(BuddyInfo aBuddy) {
-        return addressBook.remove(aBuddy);
+        return myBuddies.remove(aBuddy);
     }
 
     public static void main(String[] args) {
-        System.out.println("Address book");
+        BuddyInfo buddy = new BuddyInfo("Peter Griffin", "31 Spooner St.", "123-456-7890");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
