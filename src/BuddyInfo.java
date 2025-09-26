ba3f8eab32b0674557a900782322f97ed7589f1d
liamaddie <liamaddie@cmail.carleton.ca>
2025-09-26 14:43:39 -0400

    Just commiting my BuddyInfo class

---
+++ src/BuddyInfo.java
@@ -0,0 +1,28 @@
+public class BuddyInfo
+{
+    private String name;
+    private String address;
+    private String number;
+
+    public BuddyInfo() {
+        name = "None";
+        address = "None";
+        number = "None";
+        // this("None", "None", "None");
+    }
+
+    public BuddyInfo(String name, String address, String number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo bud = new BuddyInfo("Peter Griffin", "31 Spooner St.", "123-456-7890");
+        System.out.println("Hello " + bud.getName());
+    }
+}
